d5d98dcc7223e48945995a6350ea0feb1d8d88c2
sdh2025 <kati_huebener@yahoo.de>
2025-05-13 15:31:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ st.set_page_config(page_title="Sing Dein Herz", page_icon="🐤")
 
 st.title("🐤 Sing Dein Herz 💛")
 st.markdown("\n\n") 
-st.write("Hier entsteht schon bald etwas neues 📯 ")
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <h3>Hier entsteht schon bald etwas Neues 📯</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
