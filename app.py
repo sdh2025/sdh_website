be09a85048899147c2f717dd28ff92571a3109b3
sdh2025 <kati_huebener@yahoo.de>
2025-05-13 15:29:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="Sing Dein Herz", page_icon="🐤")
 
 st.title("🐤 Sing Dein Herz 💛")
-st.write("   Hier entsteht schon bald etwas neues 📯 ")
+st.markdown("\n\n") 
+st.write("Hier entsteht schon bald etwas neues 📯 ")
