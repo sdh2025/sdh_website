8b92143c4da97195d52e1dc0aca507f79c05e24d
sdh2025 <kati_huebener@yahoo.de>
2025-07-21 14:59:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,8 +61,8 @@ st.markdown("**TAYOME**, Stierstraße 1, 12159 Berlin Friedenau (www.tayome.de)"
 st.subheader("🌿 Termine - Singen in Friedenau ")
 termine = [
     "So 27. Juli 2025              / 18:00h-19:30h (Warteliste)",
-    "So 10. August 2025            / 18:00h-19:30h",
-    "So 07. September 2025         / 18:00h-19:30h"
+    "So 10. August 2025            / 18:00h-19:30h" (15 Plätze frei),
+    "So 07. September 2025         / 18:00h-19:30h" 
 ]
 
 for datum in termine:
